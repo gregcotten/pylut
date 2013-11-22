10bc13cc03d888b955aaf1e2ca03d118f7d03e45
Greg Cotten <gregcotten@gmail.com>
2013-11-21 18:52:39 -0800

    new version

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 
 setup(
     name='pylut',
-    version='1.0.0',
+    version='1.1.0',
     author='Greg Cotten',
     author_email='gcgc90@gmail.com',
     packages=['pylut'],
     scripts=['bin/basic_example.py'],
     url='http://pypi.python.org/pypi/PyLUT/',
     license='LICENSE.txt',
-    description='Manipulates 3D LUTs',
+    description='Builds, modifies, and converts 3D LUTs from popular .cube and .3dl formats.',
     long_description=open('README.txt').read(),
     install_requires=[
         "numpy"
