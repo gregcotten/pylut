e066e3bf87f795cd7e5eae890118fa011c0d3868
Greg Cotten <gregcotten@gmail.com>
2013-11-25 15:59:09 -0800

    new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='pylut',
-    version='1.2.0',
+    version='1.3.0',
     author='Greg Cotten',
     author_email='gcgc90@gmail.com',
     packages=['pylut'],
