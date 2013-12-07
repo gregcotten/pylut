526463eb36d2cd81732e1900c894b81294044c99
Greg Cotten <gregcotten@gmail.com>
2013-12-06 17:32:14 -0800

    added FromRGBIntegerArray

--- pylut/pylut.py
+++ pylut/pylut.py
@@ -88,6 +88,14 @@ class Color:
 		"""
 		return Color(array[0], array[1], array[2])
 
+	@staticmethod
+	def FromRGBIntegerArray(array, bitdepth):
+		"""
+		Creates Color from a list or tuple of 3 floats.
+		"""
+		maxBits = 2**bitdepth - 1
+		return Color(RemapIntTo01(array[0], maxBits), RemapIntTo01(array[1], maxBits), RemapIntTo01(array[2], maxBits))
+
 	
 	def ToFloatArray(self):
 		"""
